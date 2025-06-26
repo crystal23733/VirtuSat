d10e03f6bd23311f9bade396ff769168803cc12c
Yoo Hoyeong <crystal23733@gmail.com>
2025-06-27 07:40:37 +0900

    Feat|지상국 명령 입력 및 위성과 통신하는 기본 구조 구현
    - 사용자로부터 명령어를 입력 받음
    - 위성과 TCP 연결 후 명령어 전송
    - 응답을 받아 콘솔에 출력
    - 유효하지 않은 명령어는 거부
    - 네트워크와 프로토콜 모듈 사용 연동 확인

---
+++ src/main_ground.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include "network.h"
+#include "protocol.h"
+
+int main() {
+    char input[BUFFER_SIZE];
+    char response[BUFFER_SIZE];
+
+    printf("VirtuSat 지상국 작동 시작...\n");
+
+    // [1] 위성과 연결 시도
+    int sock = connect_to_server();
+
+    while(1) {
+        // [2]명령어 입력
+        printf(">> 명령 입력 (TEMP. VOLT, STAT, PING): ");
+        fgets(input, sizeof(input), stdin);
+
+        // [3] 문자열 끝 줄바꿈 제거
+        input[strcspn(input, "\n")] = '\0';
+
+        // [4] 명령어 유효성 검사
+        CommandType cmd = parse_command(input);
+        if (cmd == CMD_UNKNOWN) {
+            printf("[지상국] 잘못된 명령어입니다.\n");
+            continue;
+        }
+
+        // [5] 위성에 명령 전송
+        if (send_message(sock, input) < 0) {
+            printf("[지상국] 명령 전송 실패\n");
+            break;
+        }
+
+        // [6] 위성 응답 수신
+        if (receive_message(sock, response, BUFFER_SIZE) < 0) {
+            printf("[지상국] 응답 수신 실패!\n");
+            break;
+        }
+
+        // [7] 응답 출력
+        printf("[응답] %s\n", response);
+
+    }
+
+    return 0;
+}
