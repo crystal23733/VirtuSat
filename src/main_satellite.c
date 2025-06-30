0aeedf04eab5e2e499dbc32b149c0412e8e15d44
Yoo Hoyeong <crystal23733@gmail.com>
2025-06-30 17:52:03 +0900

    Feat|지상국과 위성 간 TCP 통신 테스트 구현 (소켓 연결 및 메시지 송수신)
    - start_server()로 실행 후 accept_connection()을 통해 지상국 연결 수락
    - receive_message()로 명령 1회 수신 및 출력 (테스트용)

--- src/main_satellite.c
+++ src/main_satellite.c
@@ -3,11 +3,29 @@
 #include <time.h>
 #include <unistd.h>
 #include "sensor.h"
+#include "network.h"
+#include "protocol.h"
 
 int main() {
     srand(time(NULL));
 
     printf("위성 작동 시작...\n");
+
+    // [1] 서버 시작
+    int server_fd = start_server();
+
+    // [2] 지상국 연결 기다리기
+    int client_fd = accept_connection(server_fd);
+
+    // [3] 1회용 수신 테스트
+    char buffer[BUFFER_SIZE];
+    int received = receive_message(client_fd, buffer, BUFFER_SIZE);
+
+    if (received > 0) {
+        printf("[위성] 받은 명령: %s\n", buffer);
+    } else {
+        printf("[위성] 수신 실패\n");
+    }
 
     while(1) {
         // 센서 값 생성
